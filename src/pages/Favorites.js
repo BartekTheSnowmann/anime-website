1467064b3b8d46b17deda727a8e7a60288225410
HenryBalon <115709320+HenryBalon@users.noreply.github.com>
2023-04-27 20:44:53 +0200

    added edit option to favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react'
-import { AiFillDelete } from 'react-icons/ai';
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 import FavoriteContext from '../context/FavoriteContext'
 import { motion } from 'framer-motion';
 
@@ -27,6 +27,9 @@ function Favorites({theme}) {
     }
   }
 
+  const [isEditing, setIsEditing] = useState(false)
+
+
   return (
     <div>
       <motion.div
@@ -34,6 +37,12 @@ function Favorites({theme}) {
       initial='hidden'
       animate='show'
       exit='hidden'>
+        <div className='max-w-[1240px] mx-auto px-4 pt-4 flex gap-x-4 text-white'>
+          <button className='bg-primary flex gap-x-2 items-center'
+          onClick={()=>setIsEditing(true)} >Edit <AiFillEdit size={20} /> </button>
+          <button className='bg-primary'
+          onClick={()=>setIsEditing(false)}>Cancel</button>
+        </div>
         <div className='max-w-[1240px] mx-auto grid grid-cols-3 sm:grid-cols-4 xl:grid-cols-5 p-4 gap-6 md:gap-8 font-bold'>
           {favoriteArray.length === 0 ? <h1 className={theme === 'Light'?'text-3xl text-dark':'text-3xl text-white'}>List is empty</h1>:
           (
@@ -41,15 +50,15 @@ function Favorites({theme}) {
               (
                 <div className='overflow-hidden group bg-white relative group item_shadow'
                 key={item.title}>
-                  <img src={item.img} alt='' className='h-full w-full group-hover:scale-105 duration-300'/>
+                  <img src={item.img} alt='' className='h-full w-full'/>
                   <div className='hidden group-hover:flex absolute top-0 h-full w-full bg-dark/50 text-white items-center justify-center'>
-                    <h1 className='drop-shadow-xl text-sm sm:text-lg xl:text-xl px-2'>{item.title? (item.title):(item.name)}</h1>
+                    <h1 className='drop-shadow-xl text-sm sm:text-lg xl:text-xl px-2  duration-300 transition-all'>{item.title? (item.title):(item.name)}</h1>
                   </div>
-                  <div 
-                  className='cursor-pointer absolute bottom-2 left-2'
+                   {isEditing  && <div 
+                  className='cursor-pointer absolute bottom-0 left-0 bg-gray-400 hover:bg-primary duration-300 p-2'
                   onClick={()=>deleteFromFavorites(item.id)}>
-                  <AiFillDelete size={40} className='text-white hidden group-hover:block'/>
-                  </div>
+                  <AiFillDelete size={25} className='text-white'/>
+                  </div>}
                 </div>
               )))
           )
